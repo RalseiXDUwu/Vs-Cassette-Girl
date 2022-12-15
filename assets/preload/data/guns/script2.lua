b2ef0d16f744be8761694522717e6dc4473c3fd1
RalseiXDUwu <96271585+RalseiXDUwu@users.noreply.github.com>
2022-12-15 17:09:54 -0500

    Initial commit

---
+++ assets/preload/data/guns/script2.lua
@@ -0,0 +1,11 @@
+function onUpdate()
+ 
+   if curStep == 879 then -- timing
+
+   doTweenY('dadTweenY', 'dad', -5000, 5.1, 'cubeIn')
+end
+   if curStep == 1020 then -- timing
+
+   doTweenY('boyfriendTweenY', 'boyfriend', -5000, 5.1, 'cubeIn')
+end
+end
